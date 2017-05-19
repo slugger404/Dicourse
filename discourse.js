f9ff1e2908f0ce1e6344919dca1585801aa24a5a
Nick Moore <moore.1474@gmail.com>
2017-05-18 21:01:55 -0700

    Added recent episodes to scope.

--- discourse.js
+++ discourse.js
@@ -1,4 +1,7 @@
 var app = angular.module('discourse', [ 'ngSanitize' ]);
+
+var NUMBER_OF_EPISODES_IN_RECENT = 10;
+
 app.controller('discourse_rss', function($scope, $http) {
 
 	$http.get("https://discourseofhumanevents.libsyn.com/rss").then(
@@ -7,13 +10,16 @@ app.controller('discourse_rss', function($scope, $http) {
 				var x2js = new X2JS();
 				var xml = response.data;
 				$scope.rssJson = x2js.xml_str2json(xml);
-				var episodes = $scope.rssJson.rss.channel.item;
-				$scope.episodes = episodes;
+				$scope.episodes = $scope.rssJson.rss.channel.item;
+				$scope.recentEpisodes = [];
 				
 				//Iterate through and number the Episodes
-				var episodeNumber = episodes.length;
-				episodes.forEach(function(episode){
+				var episodeNumber = $scope.episodes.length;
+				$scope.episodes.forEach(function(episode){
 					episode.number = episodeNumber;
+					if(episodeNumber>$scope.episodes.length-NUMBER_OF_EPISODES_IN_RECENT){
+						$scope.recentEpisodes.push(episode);	
+					}					
 					episodeNumber--;
 				})
 			});
