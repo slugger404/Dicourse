72bf88308efae1eacf46206c886ef89ec3967fcc
moore1474 <moore.1474@gmail.com>
2017-05-27 16:21:40 -0700

    Cleaned up code & Removed some boiler plate.

--- discourse.js
+++ discourse.js
@@ -4,24 +4,53 @@ var NUMBER_OF_EPISODES_IN_RECENT = 10;
 
 app.controller('discourse_rss', function($scope, $http) {
 
-	$http.get("https://discourseofhumanevents.libsyn.com/rss").then(
-			function(response) {
-				//Convert to JSON
-				var x2js = new X2JS();
-				var xml = response.data;
-				$scope.rssJson = x2js.xml_str2json(xml);
-				$scope.episodes = $scope.rssJson.rss.channel.item;
-				$scope.recentEpisodes = [];
-				
-				//Iterate through and number the Episodes
-				var episodeNumber = $scope.episodes.length;
-				$scope.episodes.forEach(function(episode){
-					episode.number = episodeNumber;
-					if(episodeNumber>$scope.episodes.length-NUMBER_OF_EPISODES_IN_RECENT){
-						$scope.recentEpisodes.push(episode);	
-					}					
-					episodeNumber--;
-				})
-			});
+	$http.get("https://discourseofhumanevents.libsyn.com/rss").then(function(response) {
+
+		convertXmlRssToJson(response, $scope);
+		applyEpisodeAdjustments($scope);
+
+	});
+
+	function convertXmlRssToJson(response, $scope) {
+		var x2js = new X2JS();
+		var xml = response.data;
+		$scope.rssJson = x2js.xml_str2json(xml);
+		$scope.episodes = $scope.rssJson.rss.channel.item;
+		$scope.recentEpisodes = [];
+	}
+
+	function applyEpisodeAdjustments($scope) {
+
+		for (i = $scope.episodes.length - 1; i >= 0; i--) {
+			
+			//Most recent are at top of array
+			index = $scope.episodes.length - i - 1;
+			episode = $scope.episodes[index]
+			
+			applyEpisodeNumberAdjustment(episode);			 
+			addRecentEpisodesAdjustment($scope, episode, i, $scope.episodes.length);
+			stripBoilerPlate(episode);
+			
+		}
+
+	}
+
+	function addRecentEpisodesAdjustment($scope, episode, episodeNumber, numberOfEpisodes) {
+		if ($scope.episodes.length - episodeNumber <= NUMBER_OF_EPISODES_IN_RECENT) {
+			$scope.recentEpisodes.push(episode);
+		}
+	}
+
+	function applyEpisodeNumberAdjustment(episode) {
+		var titleSplit = episode.title.split('-');
+		episode.number = titleSplit[0].replace('DHE #','').trim();
+		episode.title = titleSplit[1].trim();
+	}
+	
+	function stripBoilerPlate(episode){
+		episode.description = episode.description.split("itunes.apple")[0];
+		var n = episode.description.lastIndexOf('<p><a href');
+		episode.description = episode.description.substring(0, n);
+	}
 
 });
