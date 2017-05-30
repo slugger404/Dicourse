0a4244a80d038acb4a57db9aa1b12336c5d18780
moore1474 <moore.1474@gmail.com>
2017-05-29 19:53:41 -0700

    Readding hashbangs back to URL.

--- js/router.js
+++ js/router.js
@@ -1,20 +1,7 @@
-app.config([ '$locationProvider', function($locationProvider) {
-	$locationProvider.hashPrefix('');
-	$locationProvider.html5Mode({
-		enabled : true,
-		requireBase : false
-	})
-	$locationProvider.html5Mode(true);
-} ]);
-
 app.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when("/", {
 		templateUrl : "pages/main.html"
-	}).when("/Discourse/", {
-		templateUrl : "pages/main.html"
 	}).when("/hosts", {
-		templateUrl : "pages/hosts.html"
-	}).when("/Discourse/hosts", {
 		templateUrl : "pages/hosts.html"
 	});    
 }]);
