e4795d576a17083ed1018d5b0fd205660537e75c
DiscourseCoin <zachmoore404@gmail.com>
2021-02-21 14:40:23 -0500

    Update episode_adjustments.js

--- js/episode_adjustments.js
+++ js/episode_adjustments.js
@@ -46,7 +46,7 @@ function convertXmlRssToJson(response, $scope) {
 	function applyEpisodeNumberAdjustment(episode) {
 		var titleSplit = episode.title.trim();
 		episode.number = titleSplit[0].replace('DHE #','').trim();
-		episode.title = titleSplit[1].trim();
+		episode.title = titleSplit[1];
 	}
 	
 	function stripBoilerPlateAdjustment(episode){
